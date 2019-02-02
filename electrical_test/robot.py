b4583c6345ef504a710c7808615c786d086d0377
AndrewLester <alester220@gmail.com>
2019-02-02 12:28:57 -0500

    Add encoders to electrical test

--- electrical_test/robot.py
+++ electrical_test/robot.py
@@ -3,9 +3,13 @@
 import magicbot
 import wpilib
 import wpilib.drive
+import math
 
 
 class TestRobot(magicbot.MagicRobot):
+    ENCODER_PULSE_PER_REV = 1024
+    WHEEL_DIAMETER = 0.5
+
     def createObjects(self):
         """
         Initialize testbench components.
@@ -13,12 +17,23 @@ class TestRobot(magicbot.MagicRobot):
         self.joystick = wpilib.Joystick(0)
         self.lift_motors = [wpilib.Victor(i) for i in range(4, 8)]
 
+        encoder_constant = (
+            (1 / self.ENCODER_PULSE_PER_REV) * self.WHEEL_DIAMETER * math.pi
+        )
+
+        self.l_encoder = wpilib.Encoder(2, 3)
+        self.l_encoder.setDistancePerPulse(encoder_constant)
+        self.r_encoder = wpilib.Encoder(0, 1)
+        self.r_encoder.setDistancePerPulse(encoder_constant)
+
     def teleopPeriodic(self):
         """
         Spin all motors at full speed.
         """
-        for motor in self.lift_motors:
-            motor.set(self.joystick.getY())
+        # for motor in self.lift_motors:
+        #     motor.set(self.joystick.getY())
+
+        print(self.r_encoder.get())
 
 
 if __name__ == '__main__':
